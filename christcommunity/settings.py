48b9614565cefedb8e0443725595d77809eefaa8
Young <43630508+younGsse@users.noreply.github.com>
2021-03-17 15:21:01 +0900

    Update settings.py

--- christcommunity/settings.py
+++ christcommunity/settings.py
@@ -80,8 +80,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'USER': 'christYoung',
-        'PASSWORD': 'keyChrist!',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
