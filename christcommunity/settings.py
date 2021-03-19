d5001cc71965e28d8d3a5a23318b24579980d9b3
Young <43630508+younGsse@users.noreply.github.com>
2021-03-19 22:01:27 +0900

    Update settings.py

--- christcommunity/settings.py
+++ christcommunity/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oukfdpk$2jwswg+hew&y8!ax2w6w26ilxec!7xm(9l)=ghsugi'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
